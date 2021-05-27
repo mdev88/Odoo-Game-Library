96bd804cd37222f21295663caf0e02da0f5c16d3
Martín Vukovic <18-martin@users.noreply.gitlab.nubeprint.net>
2021-05-27 13:25:02 -0300

    Computed field status

--- models/game.py
+++ models/game.py
@@ -1,9 +1,18 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Game(models.Model):
     _name = 'game.model'
     _description = 'Game model'
+    _order = 'name'
+
+    @api.depends('completed')
+    def get_status(self):
+        for rec in self:
+            if rec.completed:
+                rec.status = 'Completed'
+            else:
+                rec.status = 'Not completed'
 
     name = fields.Char('Title', required=True)
     year = fields.Char('Year', help='Release date')
@@ -15,5 +24,6 @@ class Game(models.Model):
                               ('strategy', 'Strategy'),
                               ('other', 'Other')])
     completed = fields.Boolean('Completed')
+    status = fields.Char('', compute='get_status')
     cover = fields.Binary('Cover')
     wikipedia = fields.Char('Wikipedia')
